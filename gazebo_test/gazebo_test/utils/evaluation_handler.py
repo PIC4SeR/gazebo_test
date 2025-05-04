b6608981373c2468cc731d27cccdd5ca6f5ed55b
Andrea Ostuni <andrea.ostuni@polito.it>
2025-05-04 18:15:31 +0000

    refactor: update ExperimentResult enum and streamline collision handling in ExperimentEvaluator

--- gazebo_test/gazebo_test/utils/evaluation_handler.py
+++ gazebo_test/gazebo_test/utils/evaluation_handler.py
@@ -20,7 +20,7 @@ class ExperimentResult(Enum):
     FAILURE_COLLISION_ENVIRONMENT = 2
     FAILURE_COLLISION_AGENT = 3
     FAILURE_TIMEOUT = 4
-    FAILURE_UNKNOWN = 5
+    RESULT_NOT_SET = 5
 
     def __str__(self):
         match self:
@@ -32,10 +32,8 @@ class ExperimentResult(Enum):
                 return "Failure: Collision with agent"
             case ExperimentResult.FAILURE_TIMEOUT:
                 return "Failure: Timeout"
-            case ExperimentResult.FAILURE_UNKNOWN:
-                return "Failure: Unknown error"
-            case _:
-                return "Unknown result"
+            case ExperimentResult.RESULT_NOT_SET:
+                return "Result not set"
 
 
 class ExperimentEvaluator:
@@ -49,18 +47,15 @@ class ExperimentEvaluator:
         self.success_event = asyncio.Event()
         self.collision_event = asyncio.Event()
         self.timeout_event = asyncio.Event()
-        self.collision_with_agent = False
 
-        self.collision_with_environment = False
-        self.is_timeout = False
+        self.experiment_result = ExperimentResult.RESULT_NOT_SET
 
         node.create_subscription(
-            Collision, "/jackal/collision", self.collision_callback, 10
+            Collision, "/jackal/collision", self._collision_callback, 10
         )
         self.logger = rclpy.logging.get_logger("experiment_evaluator")
         self.get_clock = node.get_clock
         self.timeout_timer = None
-        self.is_running = False
 
     async def run_experiment(self) -> ExperimentResult:
         """
@@ -74,17 +69,14 @@ class ExperimentEvaluator:
         Returns:
         - ExperimentResult: The result of the experiment
         """
-        self.is_running = True
         loop = asyncio.get_running_loop()
         self._loop = loop
         self.start_time = self.get_clock().now()
-        self.collision_with_agent = False
-        self.collision_with_environment = False
-        self.is_timeout = False
-        self.logger.info("Experiment started")
-        self.logger.info(f"Timeout set to {self.timeout_duration} seconds", once=True)
-        self.logger.info(f"Start time: {self.start_time}")
-        self.create_timeout_timer()
+        self.experiment_result = ExperimentResult.RESULT_NOT_SET
+        self.logger.debug("Experiment started")
+        self.logger.debug(f"Timeout set to {self.timeout_duration} seconds", once=True)
+        self.logger.debug(f"Start time: {self.start_time}")
+        self._create_timeout_timer()
 
         # Wait for one of: collision_event, success_event, or timeout
         done, pending = await asyncio.wait(
@@ -95,9 +87,8 @@ class ExperimentEvaluator:
             ],
             return_when=asyncio.FIRST_COMPLETED,
         )
-        self.is_running = False
-        self.logger.info(f"Finish time:{self.get_clock().now()}")
-        self.logger.info(f"Elapsed time: {self.get_clock().now() - self.start_time}")
+        self.logger.debug(f"Finish time:{self.get_clock().now()}")
+        self.logger.debug(f"Elapsed time: {self.get_clock().now() - self.start_time}")
         # Cancel any pending tasks
         for task in pending:
             task.cancel()
@@ -110,17 +101,15 @@ class ExperimentEvaluator:
         self.success_event.clear()
         self.timeout_event.clear()
 
-        experiment_evaluation = self.evaluate_experiment()
-        return experiment_evaluation
+        return self.experiment_result
 
     def _on_timeout(self):
         """ROS Timer callback: timeout reached based on sim time."""
-        self.is_timeout = True
-        # Wake up experiment coroutine
+        self.experiment_result = ExperimentResult.FAILURE_TIMEOUT
         self._loop.call_soon_threadsafe(self.timeout_event.set)
-        self.logger.info("Timeout reached")
+        self.logger.debug("Timeout reached")
 
-    def collision_callback(self, msg: Collision):
+    def _collision_callback(self, msg: Collision):
         """Callback for the collision sensor.
         If a collision is detected, set the collision_event.
         Then verify if the collision is with an agent or the environment.
@@ -145,17 +134,17 @@ class ExperimentEvaluator:
         self.logger.debug(f"Collision with: {msg.objects_hit}")
         self.logger.debug(f"Collision time: {msg.header.stamp}")
         if "agent" in msg.objects_hit[0]:
-            self.collision_with_agent = True
+            self.experiment_result = ExperimentResult.FAILURE_COLLISION_AGENT
             self.logger.debug("Collision with agent detected")
         else:
-            self.collision_with_environment = True
+            self.experiment_result = ExperimentResult.FAILURE_COLLISION_ENVIRONMENT
             self.logger.info("Collision with environment detected")
         # Stop the experiment
         self.logger.debug("Stopping the experiment")
         # notify the experiment that a collision has been detected using asyncio event
         self._loop.call_soon_threadsafe(self.collision_event.set)
 
-    def create_timeout_timer(self):
+    def _create_timeout_timer(self):
         """Create a timer to check for timeout."""
         if self.timeout_timer:
             self.timeout_timer.reset()
@@ -165,12 +154,8 @@ class ExperimentEvaluator:
             self._on_timeout,
         )
 
-    def evaluate_experiment(self) -> ExperimentResult:
-        """Evaluate the experiment and return the result."""
-        if self.is_timeout:
-            return ExperimentResult.FAILURE_TIMEOUT
-        if self.collision_with_agent:
-            return ExperimentResult.FAILURE_COLLISION_AGENT
-        elif self.collision_with_environment:
-            return ExperimentResult.FAILURE_COLLISION_ENVIRONMENT
-        return ExperimentResult.SUCCESS
+    def set_success_event(self):
+        """Set the success event."""
+        self.experiment_result = ExperimentResult.SUCCESS
+        self._loop.call_soon_threadsafe(self.success_event.set)
+        self.logger.debug("Success event set")
